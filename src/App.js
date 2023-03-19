7d459e0aa16c9406f3346210743f95bac9386d9b
alin <plushytowel@gmail.com>
2023-03-19 12:02:58 +0200

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import Searchbox from './Searchbox';
+import Scroll from './Scroll';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+        robots: [],
+        searchfield: ''
+       } 
+    }
+
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users }));
 }
 
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render () {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <Searchbox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots = {filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    };
+};
+
 export default App;
+
+//App-Parent and the Childs are Searchbox and CardList
+//Anytime the searchBox changes - onChange, I'm going to run the function
+//callin the function searchChange (Everytime the on change event is triggerred- call the searchchange function - which is a prop)
